f6813df64db550cc227eb487207a563ec2ccc70b
KevinPires <89464152+KevinPires@users.noreply.github.com>
2022-05-29 11:59:20 -0300

    Finalizado

--- api/src/controller/filmeController.js
+++ api/src/controller/filmeController.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { alterarImagem, inserirFilme } from "../repository/filmeRepository.js";
+import { alterarFilme, alterarImagem, buscarPorId, buscarPorNome, inserirFilme, listarTodosFilmes, removerFilme } from "../repository/filmeRepository.js";
 
 import  multer  from 'multer' 
 
@@ -20,7 +20,7 @@ server.post('/filme', async (req, resp) =>{
             throw new Error('Avaliação do filme obrigatório');
         if (!novoFilme.lancamento)
             throw new Error('Lancamento do filme obrigatório');
-        if (!novoFilme.disponivel)
+        if (novoFilme.disponivel == undefined)
             throw new Error('Campo disponível é obrigatório');
         if (!novoFilme.usuario)
             throw new Error('Usuário não logado');
@@ -56,7 +56,109 @@ server.put('/filme/:id/capa', upload.single('capa'), async (req, resp)=> {
     }
 })
 
+server.get('/filme', async (req, resp)=> {
+    try{
 
- 
+        const resposta = await listarTodosFilmes();
+        resp.send(resposta);
+
+    }catch(err){
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+server.get('/filme/busca', async (req, resp) => {
+    try{
+        const { nome }  = req.query;
+
+        const resposta = await buscarPorNome(nome);
+
+        
+        if(resposta.length == 0)
+            resp.status(404).send(['filme não encontrado'])
+        else
+
+        resp.send(resposta);
+
+    }catch(err){
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+server.get('/filme/:id', async (req, resp) => {
+    try{
+        const id  = Number(req.params.id);
+        const resposta = await buscarPorId(id);
+
+        
+        if(!resposta)
+            resp.status(404).send(['filme não encontrado'])
+        else
+
+        resp.send(resposta);
+
+    }catch(err){
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+server.delete('/filme/:id', async (req,resp) => {
+    try {
+        const { id } =  req.params;
+
+        const resposta = await removerFilme(Number(id));
+        if (resposta != 1)
+            throw new Error('Filme não pode ser removido.');
+        
+        resp.status(204).send();
+    } catch (err) {
+        resp.status(400).send({
+            erro:err.message
+        })
+    }
+})
+
+
+server.put('/filme/:id', async (req, resp) => {
+    try {
+        const { id } =  req.params;
+        const filme = req.body;
+
+        if (!filme.nome)
+        throw new Error('Nome do filme obrigatório!');
+    if (!filme.sinopse)
+        throw new Error('Sinopse do filme obrigatório');
+    if (!filme.avaliacao)
+        throw new Error('Avaliação do filme obrigatório');
+    if (!filme.lancamento)
+        throw new Error('Lancamento do filme obrigatório');
+    if (filme.disponivel == undefined)
+        throw new Error('Campo disponível é obrigatório');
+    if (!filme.usuario)
+        throw new Error('Usuário não logado');
+
+        const resposta =  await alterarFilme(id, filme);
+        if (resposta != 1)
+        throw new Error('Filme não pode ser alterado');
+        
+        else
+        resp.status(204).send();
+
+    } catch (err) {
+        resp.status(400).send({
+            erro:err.message
+        })
+        
+    }
+})
+
+
+
 
 export default server;
--- api/src/repository/connection.js
+++ api/src/repository/connection.js
@@ -4,7 +4,14 @@ const con = await mysql.createConnection({
     host: process.env.MYSQL_HOST, //ip
     user: process.env.MYSQL_USER, //usuário do banco
     password: process.env.MYSQL_PWD, //senha do usuário
-    database: process.env.MYSQL_DB //nome do banco de dados
+    database: process.env.MYSQL_DB, //nome do banco de dados
+    typeCast: function (field, next) {
+        if (field.type === 'TINY' && field.length === 1) {
+            return (field.string() === '1');  
+        } else {
+            return next();
+        }
+    }
 })
 
 console.log('B.D Conectado!');
